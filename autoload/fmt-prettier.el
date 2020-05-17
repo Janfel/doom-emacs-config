1d4b070087278f9eecc3b3822baf86642c305ad3
Jan Felix Langenbach <o.hase3@gmail.com>
2020-05-17 23:47:30 +0200

    Do unspeakable things to make Prettier work.

--- autoload/fmt-prettier.el
+++ autoload/fmt-prettier.el
@@ -1,10 +1,34 @@
 ;;; ~/.config/doom-emacs/autoload/fmt-prettier.el -*- lexical-binding: t; -*-
 ;;;###if (featurep! :editor fmt +define)
+;;; TODO: Rewrite to use custom functions.
+
+(defvar prettier-format-parser-alist
+  '((js-mode         . "babel")
+    (js2-mode        . "babel")
+    (js3-mode        . "babel")
+    (typescript-mode . "typescript")
+    (css-mode        . "css")
+    (scss-mode       . "scss")
+    (less-css-mode   . "less")
+    (json-mode       . "json")
+    (graphql-mode    . "graphql")
+    (markdown-mode   . "markdown")
+    (html-mode       . "html")
+    (yaml-mode       . "yaml")))
 
 ;;;###autoload (autoload 'prettier-format-buffer "autoload/fmt-prettier" nil t)
 ;;;###autoload (autoload 'prettier-format-region "autoload/fmt-prettier" nil t)
 (formatter-define! prettier
     :program "prettier"
     :args
-    `("--tab-width" ,(number-to-string indent-level)
-      ,@(when indent-tabs-mode '("--use-tabs"))))
+    `("--print-width" ,(number-to-string fill-column)
+      "--tab-width" ,(number-to-string indent-level)
+      ;; TODO: Use --cursor-offset --range-start --range-end.
+      ,@(-some->> buffer-file-name (list "--stdin-filepath"))
+      ,@(when indent-tabs-mode '("--use-tabs"))
+      ,@(-some->> prettier-format-parser (list "--parser"))
+      ,@(when (and (not prettier-format-parser)
+                   (not buffer-file-name))
+          (-some->> (or (alist-get major-mode prettier-format-parser-alist)
+                        "babel")
+            (list "--parser")))))
