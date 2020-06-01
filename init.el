d5b1917fe2ab33d23d08cd1a003085df811a1356
Jan Felix Langenbach <o.hase3@gmail.com>
2020-06-01 20:08:36 +0200

    Activate lang/arduino.

--- init.el
+++ init.el
@@ -103,6 +103,7 @@
 
        :lang
        ;;agda              ; types of types of types of types...
+       arduino
        (cc +lsp)         ; C/C++/Obj-C madness
        clojure           ; java with a lisp
        common-lisp       ; if you've seen one lisp, you've seen them all
