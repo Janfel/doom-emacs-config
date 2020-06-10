e157b6ef9e412177d9be95fb6217cf498ea7210e
Jan Felix Langenbach <o.hase3@gmail.com>
2020-06-10 16:16:56 +0200

    Rework luaformatter to use the new flags.

--- autoload/fmt-luaformatter.el
+++ autoload/fmt-luaformatter.el
@@ -5,13 +5,20 @@
   (expand-file-name "luaformatter/config.yaml" XDG-CONFIG-HOME)
   "The configuration file for `luaformatter-format-region'.")
 
+(defun luaformatter-compute-args ()
+  "Compute arguments for `luaformatter-format-region'."
+  (let ((indent      (if indent-tabs-mode 1 indent-level))
+        (cont-indent (if indent-tabs-mode 1 indent-level)))
+    (list
+     (if indent-tabs-mode "--use-tab" "--no-use-tab")
+     (format "--indent-width=%d" indent)
+     (format "--continuation-indent-width=%d" cont-indent)
+     (format "--tab-width=%d" tab-width)
+     (when (file-readable-p luaformatter-config-file)
+       (format "--config=%s" luaformatter-config-file)))))
+
 ;;;###autoload (autoload 'luaformatter-format-buffer "autoload/fmt-luaformatter" nil t)
 ;;;###autoload (autoload 'luaformatter-format-region "autoload/fmt-luaformatter" nil t)
 (formatter-define! luaformatter
-  ;; This program doesn’t accept configuration switches.
-  ;; See https://github.com/Koihik/LuaFormatter/issues/110
-  ;; for progress on this issue.
   :program "lua-format"
-  :args
-  (when (file-readable-p luaformatter-config-file)
-    (list "--config" luaformatter-config-file)))
+  :args (luaformatter-compute-args))
