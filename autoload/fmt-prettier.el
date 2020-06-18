3619cc2a57c94a9cd224a262977a7cb03fd675cc
Jan Felix Langenbach <o.hase3@gmail.com>
2020-06-18 13:56:42 +0200

    Reorganize Prettier parsers and add php-mode.

--- autoload/fmt-prettier.el
+++ autoload/fmt-prettier.el
@@ -5,18 +5,26 @@
   "The parser that Prettier should use.")
 
 (defvar prettier-format--parser-alist
-  '((js-mode         . "babel")
-    (js2-mode        . "babel")
-    (js3-mode        . "babel")
-    (typescript-mode . "typescript")
+  '(;; HTML
+    (html-mode       . "html")
+    (markdown-mode   . "markdown")
+    ;; CSS
     (css-mode        . "css")
     (scss-mode       . "scss")
     (less-css-mode   . "less")
+    ;; JavaScript
+    (js-mode         . "babel")
+    (js2-mode        . "babel")
+    (js3-mode        . "babel")
+    ;; TypeScript
+    (typescript-mode . "typescript")
+    ;; PHP
+    (php-mode        . "php")
+    ;; JSON
     (json-mode       . "json")
-    (graphql-mode    . "graphql")
-    (markdown-mode   . "markdown")
-    (html-mode       . "html")
-    (yaml-mode       . "yaml")))
+    (yaml-mode       . "yaml")
+    ;; GraphQL
+    (graphql-mode    . "graphql")))
 
 ;;;###autoload (autoload 'prettier-format-buffer "autoload/fmt-prettier" nil t)
 ;;;###autoload (autoload 'prettier-format-region "autoload/fmt-prettier" nil t)
