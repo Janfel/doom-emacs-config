90e87c3dafff15c18fef2c117f02ca43b0117a2c
Jan Felix Langenbach <o.hase3@gmail.com>
2020-06-28 10:34:43 +0200

    Improve tangling cli.

--- cli.el
+++ cli.el
@@ -1,5 +1,8 @@
 ;;; ~/.config/doom-emacs/cli.el -*- lexical-binding: t; -*-
 
-(defcli! tangle nil
+(defcli! tangle (&optional file)
   "Tangles your literate config files."
-  (+literate-tangle-h 'force))
+  (require 'ob-tangle)
+  (org-babel-tangle-file
+   (or file
+       +literate-config-file)))
