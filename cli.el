dba9813a815e60f670bd84cfff137c80c5b8c153
Jan Felix Langenbach <o.hase3@gmail.com>
2020-06-28 14:11:42 +0200

    Remove sort-of-redundant tangle cli.

--- cli.el
+++
@@ -1,8 +0,0 @@
-;;; ~/.config/doom-emacs/cli.el -*- lexical-binding: t; -*-
-
-(defcli! tangle (&optional file)
-  "Tangles your literate config files."
-  (require 'ob-tangle)
-  (org-babel-tangle-file
-   (or file
-       +literate-config-file)))
