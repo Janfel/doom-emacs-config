88961243078621d5ccad93c038ab78ff355ed23d
Jan Felix Langenbach <o.hase3@gmail.com>
2020-07-19 18:49:29 +0200

    Split doom! block based on SYSTEM.

--- init.el
+++ init.el
@@ -110,27 +110,27 @@
 
        :lang
        ;;agda                           ; types of types of types of types...
-       arduino
-       basic
-       (cc +lsp)                        ; C/C++/Obj-C madness
-       clojure                          ; java with a lisp
-       common-lisp                      ; if you've seen one lisp, you've seen them all
+       ;;arduino                        ;
+       ;;basic                          ;
+       ;;(cc +lsp)                      ; C/C++/Obj-C madness
+       ;;clojure                        ; java with a lisp
+       ;;common-lisp                    ; if you've seen one lisp, you've seen them all
        ;;coq                            ; proofs-as-programs
        ;;crystal                        ; ruby at the speed of c
        ;;csharp                         ; unity, .NET, and mono shenanigans
        data                             ; config/data formats
        ;;(dart +flutter)                ; paint ui and not much else
-       elixir                           ; erlang done right
+       ;;elixir                         ; erlang done right
        ;;elm                            ; care for a cup of TEA?
        emacs-lisp                       ; drown in parentheses
        ;;erlang                         ; an elegant language for a more civilized age
        ;;ess                            ; emacs speaks statistics
        ;;faust                          ; dsp, but you get to keep your soul
-       fennel
+       ;;fennel                         ;
        ;;fsharp                         ; ML stands for Microsoft's Language
        ;;fstar                          ; (dependent) types and (monadic) effects and Z3
        ;;(go +lsp)                      ; the hipster dialect
-       (haskell +dante)                 ; a language that's lazier than I am
+       ;;(haskell +dante)               ; a language that's lazier than I am
        ;;hy                             ; readability of scheme w/ speed of python
        ;;idris                          ; idris
        ;;json                           ; At least it ain't XML
@@ -138,12 +138,12 @@
        ;;javascript                     ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia                          ; a better, faster MATLAB
        ;;kotlin                         ; a better, slicker Java(Script)
-       latex                            ; writing papers in Emacs has never been so fun
+       ;;latex                          ; writing papers in Emacs has never been so fun
        ;;lean                           ;
        ;;factor                         ;
        ;;ledger                         ; an accounting system in Emacs
-       (lua                             ; one-based indices? one-based indices
-        +moonscript)
+       ;;(lua                           ; one-based indices? one-based indices
+       ;; +moonscript)                  ;
        markdown                         ; writing docs for people to ignore
        ;;nim                            ; python + lisp at the speed of c
        ;;nix                            ; I hereby declare "nix geht mehr!"
@@ -160,15 +160,15 @@
        ;;php                            ; perl's insecure younger brother
        ;;plantuml                       ; diagrams for confusing people more
        ;;purescript                     ; javascript, but functional
-       (python +lsp)                    ; beautiful is better than ugly
+       ;;(python +lsp)                  ; beautiful is better than ugly
        ;;qt                             ; the 'cutest' gui framework ever
        ;;racket                         ; a DSL for DSLs
        ;;rest                           ; Emacs as a REST client
        ;;rst                            ; ReST in peace
        ;;(ruby +rails)                  ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       (rust +lsp)                      ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;(rust +lsp)                    ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala                          ; java, but good
-       scheme                           ; a fully conniving family of lisps
+       ;;scheme                         ; a fully conniving family of lisps
        sh                               ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml                            ; sml
        ;;solidity                       ; do you need a blockchain? No.
@@ -176,6 +176,24 @@
        ;;terra                          ; Earth and Moon in alignment for performance.
        ;;web                            ; the tubes
        ;;yaml                           ; JSON, but readable
+       (:cond
+        ((eq SYSTEM 'Phantom)
+         arduino
+         basic
+         (cc +lsp)
+         clojure
+         common-lisp
+         elixir
+         fennel
+         (haskell +dante)
+         latex
+         (lua +moonscript)
+         (python +lsp)
+         (rust +lsp)
+         scheme)
+        ((eq SYSTEM 'Tesla)
+         (cc +lsp)
+         python))
 
        :email
        ;;(mu4e +gmail)                  ;
