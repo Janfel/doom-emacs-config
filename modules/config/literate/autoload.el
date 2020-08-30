092c5bdfc08c08e75fffb0561430dd2b0b29a6c3
Jan Felix Langenbach <o.hase3@gmail.com>
2020-08-30 02:14:47 +0200

    Update lit-tangle-sync to better match upstream.

--- modules/config/literate/autoload.el
+++ modules/config/literate/autoload.el
@@ -15,43 +15,48 @@
 ;;;###autoload
 (defun +literate-tangle-sync-h ()
   "Tangles `+literate-config-file' if it has changed."
-  (unless (getenv "NOTANGLE")
+  (unless (getenv "__NOTANGLE")
     (print! (start "Compiling your literate config..."))
-    (print-group!
-     (require 'ob-tangle)
-     (require 'ox)
-     (let* ((start (current-time))
-            (targets
-             (seq-filter
-              (lambda (f) (file-newer-than-file-p f +literate-config-cache-file))
-              +literate-config-files)))
-       (let ((cache +literate-config-cache-file)
-             ;; Prevent unwanted entries in recentf, or formatters, or
-             ;; anything that could be on these hooks, really. Nothing
-             ;; else should be touching these files (particularly in
-             ;; interactive sessions).
-             (write-file-functions nil)
-             (before-save-hook nil)
-             (after-save-hook nil)
-             ;; Prevent infinite recursion due to recompile-on-save
-             ;; hooks later, and speed up `org-mode' init.
-             (org-mode-hook nil)
-             (org-inhibit-startup t))
-         ;; Ensure output conforms to the formatting of all doom CLIs
-         (letf! ((defun message (msg &rest args)
-                   (when msg (print! (info "%s") (apply #'format msg args)))))
-           (cl-dolist (target targets)
-             (set-buffer (find-file-noselect target))
-             ;; Tangling won't ordinarily expand #+INCLUDE directives,
-             ;; so I do it myself.
-             (org-export-expand-include-keyword)
-             (org-babel-tangle)))
-         (with-temp-file cache)
-         (if (zerop (length targets))
-             (print! (success "No files to tangle"))
-           (print! (success "Tangled %s file(s) in %.02f seconds"
-                            (length targets)
-                            (float-time (time-since start))))))))))
+    (let* ((start (current-time))
+           (targets
+            (seq-filter
+             (lambda (f) (file-newer-than-file-p f +literate-config-cache-file))
+             +literate-config-files)))
+      (if (zerop (length targets))
+          (print! (success "No files to tangle"))
+        (require 'ob-tangle)
+        (require 'ox)
+        (print-group!
+         (let ((cache +literate-config-cache-file)
+               ;; Prevent unwanted entries in recentf, or formatters, or
+               ;; anything that could be on these hooks, really. Nothing
+               ;; else should be touching these files (particularly in
+               ;; interactive sessions).
+               (write-file-functions nil)
+               (before-save-hook nil)
+               (after-save-hook nil)
+               ;; Prevent infinite recursion due to recompile-on-save
+               ;; hooks later, and speed up `org-mode' init.
+               (org-mode-hook nil)
+               (org-inhibit-startup t))
+           ;; Ensure output conforms to the formatting of all doom CLIs
+           (letf! ((defun message (msg &rest args)
+                     (when msg (print! (info "%s") (apply #'format msg args)))))
+             (cl-dolist (target targets)
+               (set-buffer (find-file-noselect target))
+               ;; Tangling won't ordinarily expand #+INCLUDE directives,
+               ;; so I do it myself.
+               (org-export-expand-include-keyword)
+               (org-babel-tangle)))
+           (with-temp-file cache)
+           (if (zerop (length targets))
+               (print! (success "No files to tangle"))
+             (print! (success "Tangled %s file(s) in %.02f seconds"
+                              (length targets)
+                              (float-time (time-since start)))))))
+        (unless doom-interactive-p
+          (print! (start "Restarting..."))
+          (throw 'exit "__NOTANGLE=1 $@"))))))
 
 (defun +literate-tangle-file-async (file &optional callback)
   "Tangle FILE asynchronously, then invoke CALLBACK with the result."
