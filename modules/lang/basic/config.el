a5e0192dc966013ec48a54c124f4c1310f657d22
Jan Felix Langenbach <o.hase3@gmail.com>
2020-09-30 04:59:07 +0200

    Change basic-mode font-lock regexps.

--- modules/lang/basic/config.el
+++ modules/lang/basic/config.el
@@ -7,6 +7,38 @@
   (modify-syntax-entry ?' "< 23"   basic-mode-syntax-table)
   (modify-syntax-entry ?/ "_ 14cn" basic-mode-syntax-table)
 
+  (defconst basic-increase-indent-keywords-bol
+    (regexp-opt '("do" "for" "function" "repeat" "sub" "while")
+                'symbols))
+  (defconst basic-keyword-regexp
+    (regexp-opt '("as" "call" "const" "declare" "def" "defbol" "defdbl" "defint"
+                  "defsng" "defstr" "dim" "do" "else" "elseif" "end" "endif"
+                  "error" "exit" "fn" "for" "function" "gosub" "goto" "if"
+                  "loop" "next" "on" "step" "repeat" "return" "shared" "sub"
+                  "then" "to" "until" "wend" "while")
+                'symbols))
+  (defconst basic-type-regexp
+    (regexp-opt '("any" "boolean" "byte" "double" "integer" "longint"
+                  "pointer" "ptr" "short" "single" "string" "ubyte"
+                  "uinteger" "ulongint" "ushort" "zstring")
+                'symbols))
+  (defconst basic-builtin-regexp
+    (regexp-opt '("and" "cls" "data" "draw" "input" "let" "mat" "mod" "not" "or"
+                  "peek" "poke" "print" "read" "restore" "troff" "tron" "xor")
+                'symbols))
+  (defconst basic-preprocessor-regexp "^\\s-*#[a-zA-Z0-9_]*")
+  (defconst basic-font-lock-keywords
+    (list (list basic-comment-regexp 0 'font-lock-comment-face)
+          (list basic-linenum-regexp 0 'font-lock-constant-face)
+          (list basic-label-regexp 0 'font-lock-constant-face)
+          (list basic-constant-regexp 0 'font-lock-constant-face)
+          (list basic-keyword-regexp 0 'font-lock-keyword-face)
+          (list basic-type-regexp 0 'font-lock-type-face)
+          (list basic-function-regexp 0 'font-lock-function-name-face)
+          (list basic-builtin-regexp 0 'font-lock-builtin-face)
+          (list basic-preprocessor-regexp 0 'font-lock-preprocessor-face)))
+
+
   (map! :map basic-mode-map "C-c C-d" #'+basic/denumber)
   (map! :map basic-mode-map :localleader
         :desc "Renumber lines" "r" #'basic-renumber
