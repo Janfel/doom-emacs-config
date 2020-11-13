e74df0cbcac3e3e9347c3fece1a3953b8e5c4561
Jan Felix Langenbach <o.hase3@gmail.com>
2020-11-13 12:53:47 +0100

    Update custom.el.

--- custom.el
+++ custom.el
@@ -5,7 +5,13 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" default))
- '(safe-local-variable-values '((eval hs-hide-level 2))))
+ '(safe-local-variable-values
+   '((eval let
+           ((auto-mode-alist
+             '(("\\.zsh_\\'" . sh-mode)
+               ("\\.ztxt\\'" . sh-mode))))
+           (set-auto-mode))
+     (eval hs-hide-level 2))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
