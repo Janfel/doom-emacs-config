77fdc7b07337d1df5bdb6985a842e0a3f195fefd
Jan Felix Langenbach <o.hase3@gmail.com>
2020-12-30 13:39:06 +0100

    Introduce shfmt-format-args.

--- autoload/fmt-shfmt.el
+++ autoload/fmt-shfmt.el
@@ -1,13 +1,16 @@
 ;;; autoload/fmt-shfmt.el -*- lexical-binding: t; -*-
 ;;;###if (featurep! :editor fmt)
 
+(defvar shfmt-format-args '("-sr"))
+
 (defun shfmt-compute-args ()
   "Compute arguments for `shfmt-format-region'."
   (nconc
    (and buffer-file-name (list "-filename" buffer-file-name))
    (when (and (derived-mode-p 'sh-mode) (bound-and-true-p sh-shell))
      (list "-ln" (symbol-name sh-shell)))
-   (list "-i" (number-to-string (if indent-tabs-mode 0 standard-indent)))))
+   (list "-i" (number-to-string (if indent-tabs-mode 0 standard-indent)))
+   shfmt-format-args))
 
 ;;;###autoload (autoload 'shfmt-format-buffer "autoload/fmt-shfmt" nil t)
 ;;;###autoload (autoload 'shfmt-format-region "autoload/fmt-shfmt" nil t)
