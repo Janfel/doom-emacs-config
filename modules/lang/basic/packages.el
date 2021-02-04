7d715da844d1530d80ca81c3ec140dc5daeb5a55
Jan Felix Langenbach <o.hase3@gmail.com>
2021-02-04 23:15:47 +0100

    Update straight.el recipe for fbasic-mode.

--- modules/lang/basic/packages.el
+++ modules/lang/basic/packages.el
@@ -6,4 +6,4 @@
   :type 'local
   :recipe '(:local-repo "/home/janfel/src/elisp/fbasic-mode/"
             ;; To prevent code caching during development.
-            :no-byte-compile t))
+            :build (:not compile)))
