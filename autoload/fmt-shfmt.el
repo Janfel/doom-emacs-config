849d4e6c2b112f87c01adb090f5e37be97fef602
Jan Felix Langenbach <o.hase3@gmail.com>
2021-02-11 18:24:33 +0100

    Update +fmt-shfmt.

--- autoload/fmt-shfmt.el
+++ autoload/fmt-shfmt.el
@@ -1,19 +1,25 @@
 ;;; autoload/fmt-shfmt.el -*- lexical-binding: t; -*-
-;;;###if (featurep! :editor fmt)
 
-(defvar shfmt-format-args '("-sr"))
+;;; shfmt - Format shell programs
+;;; https://github.com/mvdan/sh
 
-(defun shfmt-compute-args ()
-  "Compute arguments for `shfmt-format-region'."
+(defvar +fmt-shfmt-program "shfmt"
+  "The executable for `+fmt-shfmt-format-region'.")
+
+(defvar +fmt-shfmt-args '("-sr")
+  "List of additional arguments passed to `+fmt-shfmt-program'.")
+
+(defun +fmt-shfmt-compute-args ()
+  "Compute arguments passed to `+fmt-shfmt-program'."
   (nconc
    (and buffer-file-name (list "-filename" buffer-file-name))
    (when (and (derived-mode-p 'sh-mode) (bound-and-true-p sh-shell))
      (list "-ln" (symbol-name sh-shell)))
    (list "-i" (number-to-string (if indent-tabs-mode 0 standard-indent)))
-   shfmt-format-args))
+   +fmt-shfmt-args))
 
-;;;###autoload (autoload 'shfmt-format-buffer "autoload/fmt-shfmt" nil t)
-;;;###autoload (autoload 'shfmt-format-region "autoload/fmt-shfmt" nil t)
-(+fmt-define shfmt
-  :program "shfmt"
-  :args (shfmt-compute-args))
+;;;###autoload (autoload '+fmt-shfmt-format-buffer "autoload/fmt-shfmt" nil t)
+;;;###autoload (autoload '+fmt-shfmt-format-region "autoload/fmt-shfmt" nil t)
+(+fmt-define +fmt-shfmt
+  :program +fmt-shfmt-program
+  :args (+fmt-shfmt-compute-args))
