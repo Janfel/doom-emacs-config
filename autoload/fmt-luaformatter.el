5da2de93b5e0328af9391765f5c71d5e00ecf527
Jan Felix Langenbach <o.hase3@gmail.com>
2021-02-11 17:33:01 +0100

    Add +fmt prefix to luaformatter.

--- autoload/fmt-luaformatter.el
+++ autoload/fmt-luaformatter.el
@@ -4,27 +4,28 @@
 ;;; LuaFormatter - Code formatter for Lua
 ;;; https://github.com/Koihik/LuaFormatter
 
-(defvar luaformatter-program "lua-format"
-  "The executable for `luaformatter-format-region'.")
+(defvar +fmt-luaformatter-program "lua-format"
+  "The executable for `+fmt-luaformatter-format-region'.")
 
-(defvar luaformatter-config-file nil
-  "The configuration file for `luaformatter-format-region'.")
+(defvar +fmt-luaformatter-config-file nil
+  "The configuration file for `+fmt-luaformatter-format-region'.")
 
-(defun luaformatter-compute-args ()
-  "Compute arguments for `luaformatter-format-region'."
+(defun +fmt-luaformatter-compute-args ()
+  "Compute arguments for `+fmt-luaformatter-format-region'."
   (let ((indent      (if indent-tabs-mode 1 standard-indent))
         (cont-indent (if indent-tabs-mode 1 standard-indent)))
     (nconc
-     (when (and luaformatter-config-file (file-readable-p luaformatter-config-file))
-       (list "--config" luaformatter-config-file))
+     (when (and +fmt-luaformatter-config-file
+                (file-readable-p +fmt-luaformatter-config-file))
+       (list "--config" +fmt-luaformatter-config-file))
      (list
       (if indent-tabs-mode "--use-tab" "--no-use-tab")
       "--indent-width"               (number-to-string indent)
       "--continuation-indent-width"  (number-to-string cont-indent)
       "--tab-width"                  (number-to-string tab-width)))))
 
-;;;###autoload (autoload 'luaformatter-format-buffer "autoload/fmt-luaformatter" nil t)
-;;;###autoload (autoload 'luaformatter-format-region "autoload/fmt-luaformatter" nil t)
-(+fmt-define luaformatter
-  :program luaformatter-program
-  :args (luaformatter-compute-args))
+;;;###autoload (autoload '+fmt-luaformatter-format-buffer "autoload/fmt-luaformatter" nil t)
+;;;###autoload (autoload '+fmt-luaformatter-format-region "autoload/fmt-luaformatter" nil t)
+(+fmt-define +fmt-luaformatter
+  :program +fmt-luaformatter-program
+  :args (+fmt-luaformatter-compute-args))
